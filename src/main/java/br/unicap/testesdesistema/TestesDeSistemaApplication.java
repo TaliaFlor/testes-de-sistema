037f7e01730d0b3373cf080faf47a3f931c22024
TaliaFlor <talianadegia@gmail.com>
2022-06-05 20:56:43 -0300

    initial commit

---
+++ src/main/java/br/unicap/testesdesistema/TestesDeSistemaApplication.java
@@ -0,0 +1,13 @@
+package br.unicap.testesdesistema;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestesDeSistemaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestesDeSistemaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/br/unicap/testesdesistema/TestesDeSistemaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.unicap.testesdesistema;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestesDeSistemaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
